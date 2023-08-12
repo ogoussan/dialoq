ffb928bf7a68860a6bbfab1ae48044b84111a80c
Bruno Ogoussan <sherif.ogoussan@teclead-ventures.de>
2023-08-12 15:47:31 +0200

    feat: string utils

--- apps/frontend/src/components/LessonsWidget/LessonCard.tsx
+++ apps/frontend/src/components/LessonsWidget/LessonCard.tsx
@@ -16,6 +16,7 @@ import {
 import React, { ReactElement } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { LessonCardMenu } from './LessonCardMenu';
+import { CamelCaseToTitleCase } from '@dialoq/utils';
 
 const LessonCard = ({ lesson }: { lesson: Lesson }): ReactElement => {
   const navigate = useNavigate();
@@ -67,7 +68,7 @@ const LessonCard = ({ lesson }: { lesson: Lesson }): ReactElement => {
           </Box>
           <Box>
             <Text>Grammar concept: </Text>
-            <Badge>{lesson.subtopic}</Badge>
+            <Badge>{CamelCaseToTitleCase(lesson.subtopic)}</Badge>
           </Box>
         </VStack>
       </CardBody>
--- libs/utils/src/index.ts
+++ libs/utils/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/ArrayUtil';
+export * from './lib/StringUtils';
