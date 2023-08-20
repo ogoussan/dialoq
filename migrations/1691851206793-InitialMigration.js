040cbabeaf92306ccc9eda6e2eae015730d74b87
Bruno Ogoussan <sherif.ogoussan@teclead-ventures.de>
2023-08-20 18:17:47 +0200

    chore: migration update

--- migrations/1691851206793-InitialMigration.js
+++ migrations/1691851206793-InitialMigration.js
@@ -21,7 +21,7 @@ module.exports = class InitialMigration1691851206793 {
       `CREATE TYPE "public"."lesson_language_enum" AS ENUM('german', 'french', 'spanish')`
     );
     await queryRunner.query(
-      `CREATE TABLE "lesson" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "name" character varying NOT NULL, "userId" uuid NOT NULL, "language" "public"."lesson_language_enum" NOT NULL, "theme" character varying NOT NULL, "subtopic" character varying NOT NULL, CONSTRAINT "PK_0ef25918f0237e68696dee455bd" PRIMARY KEY ("id"))`
+      `CREATE TABLE "lesson" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "name" character varying NOT NULL, "userId" uuid NOT NULL, "language" "public"."lesson_language_enum" NOT NULL, "theme" character varying NOT NULL, "subtopic" character varying NOT NULL, "topic" character varying NOT NULL, CONSTRAINT "PK_0ef25918f0237e68696dee455bd" PRIMARY KEY ("id"))`
     );
     await queryRunner.query(
       `ALTER TABLE "task" ADD CONSTRAINT "FK_dec5819f5bdcc8b9344794aa624" FOREIGN KEY ("lessonId") REFERENCES "lesson"("id") ON DELETE CASCADE ON UPDATE NO ACTION`
